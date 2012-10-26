68097ff2f8e85eb3f0e4453eaaae2546feefe72f
Ben Postlethwaite <post.ben.here@gmail.com>
2012-10-26 10:52:36 -0700

    Add meshfunc function with return a function for building graded meshes.
    meshfunc has an interactive mode for dynamically choosing mesh style
    and has decent documentation in the function description.
    It has been tested in a few cases, more testing might be required.

--- tests.m
+++ tests.m
@@ -1,18 +1,46 @@
 % Graded mesh
 clear all
 close all
+addpath vectorize
 
-n = 100;
-x = linspace(2, 6 , n);
 
-weight = 5;
+n = 50;
+
+curve = 4;
+w = 0.1;
 % Gmesh builds a fat parabola with minimum dictated by wieght
-gmesh = @(x) ((x - (x(end) - x(1))/2 - x(1)).^4 + (x(end) - x(1)) / (length(x) / weight));
+% The w/(1-w) is necessary to that the scaling is preserved.
+gmesh = @(x) ((x - (x(end) - x(1))/2 - x(1)).^curve + (w / (1-w) ) * ((x(end) - x(1))/2).^curve);
 % getdx turns gmesh into an x and dx
 getdx = @(x) (x(end) - x(1)) * gmesh(x) / sum(gmesh(x));
+%getdx = @(x) gmesh(x);
 getx = @(x) cumsum([x(1), getdx(x)]);
+% Project cell walls to centres
+centre = @(x) x(1:end - 1) + 0.5*diff(x);
+% Get dims performs a few function calls to make things easier.
+getdims = @(x) deal( getx(centre(x)), getdx(centre(x)) );
 
-dx = getdx(x);
-x = getx(x);
 
-plot(dx)
+
+gd = meshfunc(4, 0.1, true);
+
+x0 = linspace(2, 6 , n);
+
+[x, dx] = gd( x0 );
+xc = centre(x);
+[x2, dx2] = getdims( x0 );
+
+norm(x - x2)
+
+
+
+
+fprintf('Ratio of dx(centre) / dx(end) = %f\n', dx(round(n/2))/dx(end))
+
+%plot(xc, [dx], 'r*')
+%line([x; x], [ones(n,1)' * min(dx) ; ones(n,1)' *max(dx)],'Color',[.8 .8 .8])
+%xlim( [min(x) - 0.01*min(x), max(x) + 0.01*min(x)] )
+%ylim( [min(dx), max(dx) ] )
+%title('Graded mesh')
+%legend('Calculated dx','mesh line \rho')
+
--- vectorize/forwardOpTest.m
+++ vectorize/forwardOpTest.m
@@ -3,41 +3,39 @@ close all
 % Set up dimensions
 
 N = 6:2:40;
-%N = 10;
+N = 10;
+
+%% Graded Mesh (dumb octree)
+curve = 4;
+w = 0.1; % Percent finer mesh at finest scale to coarsest.
+
+% Project cell walls to centres
+centre = @(x) x(1:end - 1) + 0.5*diff(x);
+% Get function for creating meshes with given arguments. See meshfunc for
+% details.
+getdims = meshfunc(4, 0.1, false);
+
 % Outer Test loop, for testing computational time.
 for tt = 1:length(N)
     
-    x = linspace(5, 6, N(tt));
-    y = linspace(5, 6, N(tt) + 1);
-    z = linspace(5, 6, N(tt) - 1);
+    [z, dz] = getdims(linspace(5, 6, N(tt) - 1));
+    [x, dx] = getdims(linspace(5, 6, N(tt)));
+    [y, dy] = getdims(linspace(5, 6, N(tt) + 1));
     
-    %% Graded Mesh (dumb octree) 
-    weight = 5;
-    % Gmesh builds a fat parabola with minimum dictated by wieght
-    gmesh = @(x) ((x - (x(end) - x(1))/2 - x(1)).^4 + (x(end) - x(1)) / (length(x) / weight));
-    % getdx turns gmesh into a dx and getx does same for x
-    getdx = @(x) (x(end) - x(1)) * gmesh(x) / sum(gmesh(x));
-    getx = @(x) cumsum([x(1), getdx(x)]);
-    % Project cell walls to centres
-    centre = @(x) x(1:end - 1) + diff(x);
-  
-    dz = getdx(z);
-    z = centre(getx(z));
-    dx = getdx(x);
-    x = centre(getx(x));
-    dy = getdx(y);
-    y = centre(getx(y));
+    zc = centre(z);
+    xc = centre(x);
+    yc = centre(y);
 
-    nz = length(z);
-    nx = length(x);
-    ny = length(y);
+    nz = length(zc);
+    nx = length(xc);
+    ny = length(yc);
     nnn = nz * nx * ny;
     
     Nnn(tt) = nnn;
       
     [ObsX, ObsY] = meshgrid(5 : 1/10 : 6, 5 : 1/10 : 6);
     ObsZ = zeros(size(ObsX)) + 0.5;
-    nobs = numel(ObsX(:));
+    nobs = numel(ObsX(:));                  
     
     % Build the dz * dx * dy cell volume vector
     h = dz' * dx;
@@ -53,10 +51,10 @@ for tt = 1:length(N)
         for iy = 1 : ny
             for ix = 1 : nx
                 for iz = 1 : nz
-                    g(iz, ix, iy) = ((z(iz) - ObsZ(kk) ) * h( (iy - 1) * nz * nx + (ix - 1) * nz + iz)) / ...   % 
-                                    ((  (x(ix)-ObsX(kk))^2 +...
-                                        (y(iy) - ObsY(kk))^2 +...
-                                        (z(iz) - ObsZ(kk))^2 ).^(3/2) );
+                    g(iz, ix, iy) = ((zc(iz) - ObsZ(kk) ) * h( (iy - 1) * nz * nx + (ix - 1) * nz + iz)) / ...   % 
+                                    ((  (xc(ix)-ObsX(kk))^2 +...
+                                        (yc(iy) - ObsY(kk))^2 +...
+                                        (zc(iz) - ObsZ(kk))^2 ).^(3/2) );
                 end
             end
         end
@@ -74,9 +72,9 @@ for tt = 1:length(N)
     kronobs = @(obs) kron( e(nnn), obs(:) );
     % Kron dimensions to size G and subtract obs distance
     H = kron( e(nobs)', h);
-    Z = kron( e(nobs)', kron3( e(ny), e(nx), z)) - kronobs(ObsZ);
-    X = kron( e(nobs)', kron3( e(ny), x, e(nz))) - kronobs(ObsX);
-    Y = kron( e(nobs)', kron3( y, e(nx), e(nz))) - kronobs(ObsY);
+    Z = kron( e(nobs)', kron3( e(ny), e(nx), zc)) - kronobs(ObsZ);
+    X = kron( e(nobs)', kron3( e(ny), xc, e(nz))) - kronobs(ObsX);
+    Y = kron( e(nobs)', kron3( yc, e(nx), e(nz))) - kronobs(ObsY);
     R = (X.^2 + Y.^2 + Z.^2).^(3/2);
     G2 = Z .* H .* 1./R;
     
@@ -86,8 +84,8 @@ end
 
 fprintf('Norm of (Loop code - Vec code) is %f\n', norm(G - G2))
 %figure()
-plot(Nnn, loopt, Nnn, vect)
-legend('Loop time', 'Vectorized time')
-title('Computation Time Comparison of \nVectorized vs Loop based Forward Operator Code')
-ylabel('Time [seconds]')
-xlabel('Number of cells')
+%plot(Nnn, loopt, Nnn, vect)
+%legend('Loop time', 'Vectorized time')
+%title('Computation Time Comparison of \nVectorized vs Loop based Forward Operator Code')
+%ylabel('Time [seconds]')
+%xlabel('Number of cells')
