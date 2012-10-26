20faecbacb3cefcec04e98b4b29cb9b7b1a9f264
Ben Postlethwaite <post.ben.here@gmail.com>
2012-10-25 22:30:58 -0700

    Vectorized G operator with parabolic difference generated graded mesh.
    Using a parabolic approximation to finding a dx dy dz graded cell size
    prevents sharp jumps and is easily customizable.
    This has been developed and integrated into the vectorized
    forward operator G. It's fucking awesome.

--- tests.m
+++ tests.m
@@ -1,14 +1,18 @@
-% Symmetric matrix test
+% Graded mesh
 clear all
 close all
 
+n = 100;
+x = linspace(2, 6 , n);
 
-n = 5;
-m = 10;
-A = randi(10,[n,m]);
-x = randi(10, [m,1]);
-full(A')
+weight = 5;
+% Gmesh builds a fat parabola with minimum dictated by wieght
+gmesh = @(x) ((x - (x(end) - x(1))/2 - x(1)).^4 + (x(end) - x(1)) / (length(x) / weight));
+% getdx turns gmesh into an x and dx
+getdx = @(x) (x(end) - x(1)) * gmesh(x) / sum(gmesh(x));
+getx = @(x) cumsum([x(1), getdx(x)]);
 
-B = A' * A;
+dx = getdx(x);
+x = getx(x);
 
-full(B)
+plot(dx)
--- vectorize/forwardOpTest.m
+++ vectorize/forwardOpTest.m
@@ -2,70 +2,90 @@ clear all
 close all
 % Set up dimensions
 
-N = 6:2:10;
+N = 6:2:40;
 %N = 10;
-% Outer Test loop
+% Outer Test loop, for testing computational time.
 for tt = 1:length(N)
-    x = linspace(5, 6, N(tt))';
-    y = linspace(5, 6, N(tt) + 1)';
-    z = linspace(5, 6, N(tt) - 1)';
     
-    n1 = length(z);
-    n2 = length(x);
-    n3 = length(y);
-    nnn = n1 * n2 * n3;
+    x = linspace(5, 6, N(tt));
+    y = linspace(5, 6, N(tt) + 1);
+    z = linspace(5, 6, N(tt) - 1);
+    
+    %% Graded Mesh (dumb octree) 
+    weight = 5;
+    % Gmesh builds a fat parabola with minimum dictated by wieght
+    gmesh = @(x) ((x - (x(end) - x(1))/2 - x(1)).^4 + (x(end) - x(1)) / (length(x) / weight));
+    % getdx turns gmesh into a dx and getx does same for x
+    getdx = @(x) (x(end) - x(1)) * gmesh(x) / sum(gmesh(x));
+    getx = @(x) cumsum([x(1), getdx(x)]);
+    % Project cell walls to centres
+    centre = @(x) x(1:end - 1) + diff(x);
+  
+    dz = getdx(z);
+    z = centre(getx(z));
+    dx = getdx(x);
+    x = centre(getx(x));
+    dy = getdx(y);
+    y = centre(getx(y));
+
+    nz = length(z);
+    nx = length(x);
+    ny = length(y);
+    nnn = nz * nx * ny;
     
     Nnn(tt) = nnn;
-    
-    dx = x(2) - x(1);
-    dy = y(2) - y(1);
-    dz = z(2) - z(1);
-    
+      
     [ObsX, ObsY] = meshgrid(5 : 1/10 : 6, 5 : 1/10 : 6);
     ObsZ = zeros(size(ObsX)) + 0.5;
     nobs = numel(ObsX(:));
     
-    h = dx*dy*dz;
-    
+    % Build the dz * dx * dy cell volume vector
+    h = dz' * dx;
+    h = h(:) * dy;
+    h = h(:)';
     
     %% Loop Code
     tic
-    g = zeros(n1, n2, n3);
-    G = zeros(nnn, nobs);
+    g = zeros(nz, nx, ny);
+    G = zeros(nobs, nnn);
     count = 1;
     for kk = 1:nobs
-        for iy = 1 : n3
-            for ix = 1 : n2
-                for iz = 1 : n1
-                    g(iz, ix, iy) =((z(iz) - ObsZ(kk) )*h)*1/(((x(ix)-ObsX(kk))^2 + (y(iy) - ObsY(kk))^2 + (z(iz) - ObsZ(kk))^2).^(3/2));
+        for iy = 1 : ny
+            for ix = 1 : nx
+                for iz = 1 : nz
+                    g(iz, ix, iy) = ((z(iz) - ObsZ(kk) ) * h( (iy - 1) * nz * nx + (ix - 1) * nz + iz)) / ...   % 
+                                    ((  (x(ix)-ObsX(kk))^2 +...
+                                        (y(iy) - ObsY(kk))^2 +...
+                                        (z(iz) - ObsZ(kk))^2 ).^(3/2) );
                 end
             end
         end
-        G(:,count) = g(:);%reshape(g,[nnn,1]);
-        count = count +1;
+        G(count, :) = g(:)';
+        count = count + 1;
     end
     loopt(tt) = toc; %#ok<*SAGROW>
     
     %% Vectorized Code
     tic
-    e = @ (n) ones(n, 1);
+    e = @ (n) ones(1, n);
     % Scale up dimensions to 3D
     kron3 = @(a, b, c)  kron(a, kron(b, c));
     % Kron observations to size G
-    kronobs = @(obs) kron( e(nnn), obs(:)' );
-    % Kron dimensions to size G
-    Z = (kron( e(nobs)', kron3( e(n3), e(n2), z)) - kronobs(ObsZ));
-    X = (kron( e(nobs)', kron3( e(n3), x, e(n1))) - kronobs(ObsX));
-    Y = (kron( e(nobs)', kron3( y, e(n2), e(n1))) - kronobs(ObsY));
+    kronobs = @(obs) kron( e(nnn), obs(:) );
+    % Kron dimensions to size G and subtract obs distance
+    H = kron( e(nobs)', h);
+    Z = kron( e(nobs)', kron3( e(ny), e(nx), z)) - kronobs(ObsZ);
+    X = kron( e(nobs)', kron3( e(ny), x, e(nz))) - kronobs(ObsX);
+    Y = kron( e(nobs)', kron3( y, e(nx), e(nz))) - kronobs(ObsY);
     R = (X.^2 + Y.^2 + Z.^2).^(3/2);
-    G2 = Z * h .* 1./R;
+    G2 = Z .* H .* 1./R;
     
     vect(tt) = toc;
      
 end
 
-figure()
 fprintf('Norm of (Loop code - Vec code) is %f\n', norm(G - G2))
+%figure()
 plot(Nnn, loopt, Nnn, vect)
 legend('Loop time', 'Vectorized time')
 title('Computation Time Comparison of \nVectorized vs Loop based Forward Operator Code')
