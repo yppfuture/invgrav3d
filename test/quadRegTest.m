a9f3392bc93664665546595734ae3da037dda9ca
benkary <benjamin.kary@gmail.com>
2012-11-09 07:18:02 -0700

    renaming quadRegTest.m

--- test/quadRegTest.m
+++ test/quadRegTest.m
@@ -5,61 +5,80 @@ clear all; close all;
 % Program a quadratic regularization inversion to check forward operator
 % accuracy against analytic solutions
 
-addpath ../utils
+Lx = 50;
+Ly = 50;
+Lz = 50;
 
-N = 6:2:40;
-N = 14;
 
-%% Graded Mesh (dumb octree)
-curve = 6;
-weight = 0.1; % Percent finer mesh at finest scale to coarsest.
+nx = 50;
+ny = 50;
+nz = 50;
 
-% Project cell walls to centres
-centre = @(x) x(1:end - 1) + 0.5*diff(x);
-% Get function for creating meshes with given arguments. See meshfunc for
-% details.
-gmesh = meshfunc(curve, weight);
-% gmesh = meshgui();
-[z, dz] = gmesh(linspace(5, 6, N - 1));
-[x, dx] = gmesh(linspace(5, 6, N));
-[y, dy] = gmesh(linspace(5, 6, N + 1));
-    
-zc = centre(z);
-xc = centre(x);
-yc = centre(y);
+x = linspace(1,Lx,nx);
+y = linspace(1,Ly,ny);
+z = linspace(1,Lz,nz);
 
-plotMesh(z, x)
-   
-nz = length(zc);
-nx = length(xc);
-ny = length(yc);
-nnn = nz * nx * ny;
-   
-Nnn = nnn;
-      
-[ObsX, ObsY] = meshgrid(5 : 1/10 : 6, 5 : 1/10 : 6);
-ObsZ = zeros(size(ObsX)) + 0.5;
-nobs = numel(ObsX(:));   
+dx = Lx/nx;
+dy = Ly/ny;
+dz = Lz/nz;
+
+dv = dx*dy*dz;
+
+[X Y Z] = meshgrid(x,y,z);
+
+% When I ordered the matrices to accomodate the matlab vectorize order (
+% for when we multiply m(:).  the kronecker products required to build the
+% operator showed a slightly different symmetry than when BenP derived the
+% same problem, so the codes look a little different.
+
+kron3 = @(a,b,c,d) kron(a,kron(b,kron(c,d)));
+e = @(n) ones(1,n);
+
+% Geometric part of the Gravity kernel
+% According to the UBC Documentation, the gravity kernel involves source
+% location minus observation location, whereas the magnetic kernel
+% has the order reversed.  Below is the magnetic.
+
+% R = ((((kron3(ez,exy',obsX',ez')-kron3(ez,exy,obsX,ez)).^2)...
+% +(kron3(ez,exy',ez',obsY')-kron3(ez,exy,ez,obsY)).^2)...
+% +(kron3(1,exy',ez',obsZ)-kron3(1,exy,z,exy)).^2).^(-1/2);
+% exy = ones(1,nx*ny)
+% ez = ones(1,nz)
+% Here we have the gravity case.
+% G = (dv*(kron3(1,exy',z,exy)-kron3(1,exy,ez',obsZ))...
+% .*((((kron3(ez,exy',obsX,ez)-kron3(ez,exy,obsX',ez')).^2)...
+% +(kron3(ez,exy',ez,obsY)-kron3(ez,exy,ez',obsY')).^2)...
+% +(kron3(1,exy',z,exy)-kron3(1,exy,ez',obsZ)).^2).^(-3/2));
+
+% G = dv*(kron(e(nx*ny)',Z(:)')-zeros(nx*ny,nx*ny*nz))...
+% .*((((kron(e(nx*ny)',X(:)')-kron(e(nx*ny*nz),kron(x',e(ny)'))).^2)...
+% +((kron(e(nx*ny)',Y(:)')-kron(e(nx*ny*nz),kron(e(nx)',y'))).^2)...
+% +((kron(e(nx*ny)',Z(:)')-zeros(nx*ny,nx*ny*nz)).^2)).^(-3/2));
+
+num = (kron(e(nx*ny)',Z(:)')-zeros(nx*ny,nx*ny*nz));
+XX = ((kron(e(nx*ny)',X(:)')-kron(e(nx*ny*nz),kron(x',e(ny)'))).^2);
+YY = ((kron(e(nx*ny)',Y(:)')-kron(e(nx*ny*nz),kron(e(nx)',y'))).^2);
+ZZ = ((kron(e(nx*ny)',Z(:)')-zeros(nx*ny,nx*ny*nz)).^2);
+
+GG = 6.67e-11*num*dv./((XX+YY+ZZ).^3/2);
 
 m = zeros(nx,ny,nz);
-m(:,:,6) = 1;
-    
-% Build the dz * dx * dy cell volume vector
-h = dz' * dx;
-h = h(:) * dy;
-h = h(:)';
-    
-%%
-e = @ (n) ones(1, n);
-% Scale up dimensions to 3D
-kron3 = @(a, b, c)  kron(a, kron(b, c));
-% Kron observations to size G
-kronobs = @(obs) kron( e(nnn), obs(:) );
-% Kron dimensions to size G and subtract obs distance
-H = kron( e(nobs)', h);
-Z = kron( e(nobs)', kron3( e(ny), e(nx), zc)) - kronobs(ObsZ);
-X = kron( e(nobs)', kron3( e(ny), xc, e(nz))) - kronobs(ObsX);
-Y = kron( e(nobs)', kron3( yc, e(nx), e(nz))) - kronobs(ObsY);
-R = (X.^2 + Y.^2 + Z.^2).^(3/2);
-G2 = Z .* H .* 1./R;
-surf(reshape(G2*m(:),11,11))
+m(25:27,25:26,20) = 5000;
+
+
+d = reshape(GG*m(:),nx,ny);
+figure()
+imagesc(x,y,d)
+
+m = zeros(nx,ny,nz);
+m(25:27,25:26,30) = 5000;
+
+
+d = reshape(GG*fliplr(m(:)),nx,ny);
+figure()
+imagesc(x,y,d)
+
+
+% e = ones(nx+1,1)
+% D = spdiags([-e e],[0 1],nx,nx)
+
