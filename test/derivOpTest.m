3de61620f3f58073425c9be0d6d3caea7b88970c
benkary <benjamin.kary@gmail.com>
2012-11-21 19:27:45 -0800

    Moved the gradient function guts into the test script to see what was going on

--- test/derivOpTest.m
+++ test/derivOpTest.m
@@ -1,27 +1,56 @@
 clear all; close all;
 
+addpath ../vectFuncs
+
 centre = @(x) x(1:end - 1) + 0.5*diff(x);
-x1 = linspace(0,10,11);
+
+x1 = linspace(0,10,100);
 xc = centre(x1);
 
-dx1 = diff(x1); dx2 = dx1; dx3 = dx1;
-[X1 X2 X3] = meshgrid(xc);
-n1 = length(x1); n2 = n1; n3 = n1;
-f = sin(pi*X1).*sin(pi*X2).*sin(pi*X3);
+dx1 = diff(xc); 
+dx2 = dx1; 
+dx3 = dx1;
 
-GRAD = gradientOp(n1, n2, n3, dx1, dx2, dx3)
-
-GRAD*f(:)
+[XC1 XC2 XC3] = meshgrid(xc);
+[X1 X2 X3]    = meshgrid(x1);
 
 
-GRADf = [pi*cos(pi*X1).*sin(pi*X2).*sin(pi*X3);
-         pi*sin(pi*X1).*cos(pi*X2).*sin(pi*X3);
-         pi*sin(pi*X1).*sin(pi*X2).*cos(pi*X3)]
+n1 = length(xc); 
+n2 = n1; 
+n3 = n1;
+fc = sin(pi*XC1).*sin(pi*XC2).*sin(pi*XC3);
+f  = sin(pi*X1).*sin(pi*X2).*sin(pi*X3);
+% GRAD = gradientOp(n1, n2, n3, dx1(:), dx2(:), dx3(:));
+dx1 = dx1(:); dx2 = dx2(:); dx3 = dx3(:);
+dx1 = [dx1(1); dx1; dx1(end)];
+dx2 = [dx2(1); dx2; dx2(end)];
+dx3 = [dx3(1); dx3; dx3(end)];
 
-DIV = GRAD'*GRAD;
+e = @(n) ones(n, 1);
+kron3 = @(a, b, c)  kron(a, kron(b, c));
+D1 = kron( dx1, e(n1)') .* spdiags([-e(n1) , e(n1) ], [-1 0], n1 + 1 , n1);
+D2 = kron( dx2, e(n2)') .* spdiags([-e(n2) , e(n2) ], [-1 0], n2 + 1 , n2);
+D3 = kron( dx3, e(n3)') .* spdiags([-e(n3) , e(n3) ], [-1 0], n3 + 1 , n3);
+
+I1 = speye(n1);
+I2 = speye(n2);
+I3 = speye(n3);
+
+GRAD = [
+    kron(I3, kron(I2, D1))
+    kron(I3, kron(D2, I1))
+    kron(D3, kron(I2, I1))
+    ];
 
 
-DIVf = -3*pi*pi*sin(pi*X1).*sin(pi*X2).*sin(pi*X3);
+% 
+% GRADf = [pi*cos(pi*X1).*sin(pi*X1).*sin(pi*X1);
+%          pi*sin(pi*X1).*cos(pi*X2).*sin(pi*X3);
+%          pi*sin(pi*X1).*sin(pi*X2).*cos(pi*X3)];
 
-DIVresid = sum((DIVf-DIV*f(:)).^2);
-GRADresid = sum((GRADf-GRAD*f(:)).^2);
+LAPf = -3*pi*pi*sin(pi*XC1).*sin(pi*XC2).*sin(pi*XC3);
+
+LAPresid = norm( LAPf(:) - (GRAD'*GRAD)*fc(:) )
+% GRADresid = norm( GRADf(:)-GRAD*fc(:) );
+
+
--- vectFuncs/gradientOp.m
+++ vectFuncs/gradientOp.m
@@ -1,14 +1,14 @@
 function GRAD = gradientOp(n1, n2, n3, dx1, dx2, dx3)
 % GRADIENTOP produces a 3 dimensional gradient operator
 % Note that GRAD' is divergence.
-% Grad takes cell nodes and computes derivatives on faces (vectors)
-% GRAD' or DIV takes values on walls and computes nodes (scalars)
+% Grad takes cell centers and computes derivatives on nodes (vectors)
+% GRAD' or DIV takes values on nodes and computes centers (scalars)
 
 dx = @(d) [d(1); d; d(end)];
 
 e = @(n) ones(n, 1);
 kron3 = @(a, b, c)  kron(a, kron(b, c));
-D = @(n,k) spdiags(dx(k), 0, n + 1) .* spdiags([-e(n) , e(n) ], [-1 0], n + 1 , n);
+D = @(n,k) spdiags(1./dx(k), 0, n + 1,n) .* spdiags([-e(n) , e(n) ], [-1 0], n + 1 , n);
 
 I1 = speye(n1);
 I2 = speye(n2);
